ef77636d0f5b47de4d47453ffd41440377457639
growthcode <growthcode@gmail.com>
2018-04-23 08:12:59 -0700

    annotate models and specs

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: activities
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ActivitySerializer < ActiveModel::Serializer
   attributes :id, :title
 end
