a991de3efb2011ca01c9336f7e18b01b9099670a
growthcode <growthcode@gmail.com>
2018-04-29 16:13:33 -0700

    save folders of Vue rearranged

--- app/javascript/app/main.js
+++ app/javascript/app/main.js
@@ -2,10 +2,10 @@
 // use Vue to target elements in your existing html templates. You would
 // need to always use single file components.
 
-// import Vue from 'vue/dist/vue.esm' // Use this to be able to target elements in
 // your existing html/erb templates
 // Add <%= javascript_pack_tag 'file_name' %> to your layout
 
+// import Vue from 'vue/dist/vue.esm' // Use this to be able to target elements in
 // import Hello from 'app/components/Hello.vue'
 // document.addEventListener('DOMContentLoaded', () => {
 //   new Vue({
